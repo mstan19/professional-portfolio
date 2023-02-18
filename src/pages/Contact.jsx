f927f4ed211bb9bde05891ff3b08023255b97415
melissa <melissastan91@gmail.com>
2023-02-17 19:45:22 -0500

    contact page layout

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,105 +1,133 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { ImPhone } from "react-icons/im";
-
+import { TfiEmail } from "react-icons/tfi";
+import { GrMapLocation } from "react-icons/gr";
 
 
 const Contact = () => {
-    const [userData, setUserData] = useState({
-        name: "",
-        email: "",
-        message: ""
-    });
+	const [userData, setUserData] = useState({
+		name: "",
+		email: "",
+		message: ""
+	});
 
-    const myContactInfo = 
+	const myContactInfomations = [
 		{
-			myEmail: "melissastan91@gmail.com",
-			myLocation: "Atlanta, GA",
-			phoneNumber: "(404)447-9089",
-		};
+			info: "melissastan91@gmail.com",
+			title: "My Email:",
+			icon: <TfiEmail />,
+		},
+		{
+			info: "(404)447-9089",
+			title: "My Phone Number:",
+			icon: <ImPhone />,
+		},
+		{
+			info: "Atlanta, GA",
+			title: "Location:",
+			icon: <GrMapLocation />,
+		},
+	]
 
-    // update state based on form input changes
-    const handleInputChange = (event) => {
-        const { name, value } = event.target;
-        setUserData({ ...userData, [name]: value });
-    };
 
-    const onSubmit = async (event) => {
-        event.preventDefault();
-        try {
-          
-        } catch (e) {
-            console.error(e);
-        }
+	// update state based on form input changes
+	const handleInputChange = (event) => {
+		const { name, value } = event.target;
+		setUserData({ ...userData, [name]: value });
+	};
 
-    };
-    return (
-        <div className="container m-auto">
-            {/* <h1>Contact</h1> */}
-            <div className="grid grid-cols-1 md:grid-cols-2 ">
-                <div className="text-3xl space-y-4">
-                    <h2 className="text-3xl font-normal">My Contact Info</h2>
-                    <div>
-                        <span><ImPhone /></span><div className="text-2xl">Location: </div><p>{myContactInfo.myLocation}</p>
-                    </div>
-                    <div className="text-2xl">Email: <p>{myContactInfo.myEmail}</p></div>
-                    <div className="text-2xl">Phone Number: <p>{myContactInfo.phoneNumber}</p></div>
-                </div>
-                <form onSubmit={onSubmit} className="bg-white p-0 m-0">
-                    {/* {error ? (
-                        <div className="text-red-700 bg-red-100 text-base border-solid border border-red-700 text-center p-7 mb-2">
-                            Error: Incorrect credentials
-                        </div>
-                    ) : null} */}
-                    <div className="mb-4">
-                        <label
-                            className="block text-black-700 text-lg mb-2"
-                            htmlFor="name"
-                        >
-                            Full Name
-                        </label>
-                        <input
-                            className="appearance-none border border-black w-full py-2 px-3 text-black-700 leading-tight rounded-lg"
-                            name="name"
-                            type="text"
-                            onChange={handleInputChange}
-                        ></input>
-                    </div>
-                    <div className="mb-4">
-                        <label
-                            className="block text-black-700 text-lg mb-2"
-                            htmlFor="email"
-                        >
-                            Email Address
-                        </label>
-                        <input
-                            className="appearance-none border border-black w-full py-2 px-3 text-black-700 leading-tight rounded-lg"
-                            name="email"
-                            type="text"
-                            onChange={handleInputChange}
-                        ></input>
-                    </div>
-                    
-                    <div className="mb-6">
-                        <label
-                            className="block text-black-700 text-lg mb-2"
-                            htmlFor="message"
-                        >
-                            Message
-                        </label>
-                        <textarea rows="5" className="block appearance-none border border-black w-full p-2.5 px-3 text-black-700 leading-tight rounded-lg" name="message" id="message" type="text" onChange={handleInputChange}></textarea>
-                    </div>
-                    <div className="flex flex-col items-center justify-between">
-                        <button
-                            className="bg-red-300 w-1/2 shadow-lg rounded-lg hover:bg-red-400 text-white py-2 px-4 focus:outline-none focus:shadow-outline"
-                            type="submit"
-                        >
-                            SUBMIT
-                        </button>
-                    </div>
-                </form>
-            </div>
-        </div>
-    );
+	const onSubmit = async (event) => {
+		event.preventDefault();
+		try {
+
+		} catch (e) {
+			console.error(e);
+		}
+
+	};
+	return (
+		<div className="">
+			<h1 className="text-4xl font-normal text-center m-6">Let's Work Together!</h1>
+			<div className="container m-auto">
+				
+				<div className="grid grid-cols-1 md:grid-cols-2 ">
+					<div className="space-y-6">
+						{myContactInfomations.map((myContact) => (
+							<div className="text-2xl" key={myContact.info}>
+								<div className="flex grid grid-cols-2">
+									<div className="flex justify-center">
+										<div className="w-10 h-10 mt-4 bg-red-200 rounded-full relative flex justify-center items-center text-center">
+											<h2 className="text-xl grid place-items-center">{myContact.icon}</h2>
+										</div>
+									</div>
+								</div>
+								<div>
+									<div className="">{myContact.title}</div>
+									<div className="">{myContact.info}</div>
+								</div>
+								
+							</div>
+						))}
+					</div>
+					<form onSubmit={onSubmit} className="bg-white p-0 m-0">
+						{/* {error ? (
+							<div className="text-red-700 bg-red-100 text-base border-solid border border-red-700 text-center p-7 mb-2">
+								Error: Incorrect credentials
+							</div>
+						) : null} */}
+						<div className="mb-4">
+							<label
+								className="block text-black-700 text-lg mb-2"
+								htmlFor="name"
+							>
+								Full Name
+							</label>
+							<input
+								className="appearance-none border border-black w-full py-2 px-3 text-black-700 leading-tight rounded-lg"
+								name="name"
+								type="text"
+								onChange={handleInputChange}
+							></input>
+						</div>
+						<div className="mb-4">
+							<label
+								className="block text-black-700 text-lg mb-2"
+								htmlFor="email"
+							>
+								Email Address
+							</label>
+							<input
+								className="appearance-none border border-black w-full py-2 px-3 text-black-700 leading-tight rounded-lg"
+								name="email"
+								type="text"
+								onChange={handleInputChange}
+							></input>
+						</div>
+
+						<div className="mb-6">
+							<label
+								className="block text-black-700 text-lg mb-2"
+								htmlFor="message"
+							>
+								Message
+							</label>
+							<textarea rows="5" className="block appearance-none border border-black w-full p-2.5 px-3 text-black-700 leading-tight rounded-lg" name="message" id="message" type="text" onChange={handleInputChange}></textarea>
+						</div>
+						<div className="flex flex-col items-center justify-between">
+							<button
+								className="bg-red-300 w-1/2 shadow-lg rounded-lg hover:bg-red-400 text-white py-2 px-4 focus:outline-none focus:shadow-outline"
+								type="submit"
+							>
+								SUBMIT
+							</button>
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+		
+
+	);
 };
 export default Contact;
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,13 +1,85 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-
+import projectOnePic from "../assets/project1pic.png";
+import projectTwoPic from "../assets/project2pic.png";
+import projectThreePic from "../assets/project3pic.png";
 
 const Projects = () => {
-
-    return (
-        <div className="bg-blue-100">
-          hi projects
-        </div>
-    );
+	const projects = [
+		{
+			title: 'MeowWoof',
+			image: projectOnePic,
+			technologies: [
+				'Node.js, ',
+				'JQuery UI, ',
+				'TailwindCSS/Bootstrap, ',
+				'sendgrid, ',
+				'Mysql, ',
+				'Sequelize'
+			],
+			github: 'https://github.com/TeryKing/Project-MeowWoof',
+			deployLink: ""
+		},
+		{
+			title: 'Turnip Fest',
+			image: projectTwoPic,
+			technologies: [
+				'Sever-side API, ',
+				'JQuery UI, ',
+				'TailwindCSS ',
+			],
+			github: 'https://github.com/Yuyitax/TurnipFest',
+			deployLink: ""
+		},
+		{
+			title: 'ResuMade',
+			image: projectThreePic,
+			technologies: [
+				'MERN stack',
+			],
+			github: 'https://github.com/PeytonCast/dream-team',
+			deployLink: ""
+		},
+		// {
+		// 	title: 'Team Profile Generator',
+		// 	image: teamGeneratorPic,
+		// 	technologies: [
+		// 		'Test, ',
+		// 		'Node.js, ',
+		// 		'Inquirer',
+		// 	],
+		// 	github: 'https://github.com/mstan19/Team-Profile-Generator-MS',
+		// 	deployLink: ""
+		// },
+		// {
+		// 	title: 'Text Editor',
+		// 	image: textEditorPic,
+		// 	technologies: [
+		// 		'Program Web Application, ',
+		// 		'IDB, ',
+		// 		'Express',
+		// 	],
+		// 	github: 'https://github.com/mstan19/text-editor-ms',
+		// 	deployLink: ""
+		// },
+		// {
+		// 	title: 'Social Network',
+		// 	image: socialNetworkPic,
+		// 	technologies: [
+		// 		'Node.js, ',
+		// 		'MongoDB, ',
+		// 		'Mongoose, ',
+		// 		'Moment, ',
+		// 		'Express, ',
+		// 	],
+		// 	github: 'https://github.com/mstan19/social-network-api-ms',
+		// 	deployLink: ""
+		// }
+	]
+	return (
+		<div className="bg-blue-100">
+			hi projects
+		</div>
+	);
 };
 export default Projects;
