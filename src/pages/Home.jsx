6bb0946e893f74c39287478ba378a3af6db630df
melissa <melissastan91@gmail.com>
2023-02-17 19:47:28 -0500

    contact page layout

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -18,10 +18,10 @@ const Home = () => {
           <h1 className="text-5xl font-semibold mb-2">Melissa Stan</h1>
           <p className="text-2xl text-gray-500 font-medium">Full Stack Web Developer</p>
           <button className="mt-10">
-            <Link to="/aboutme" class="relative px-6 py-3 font-bold text-black group">
-              <span class="absolute inset-0 w-full h-full transition duration-300 ease-out transform -translate-x-2 -translate-y-2 bg-red-300 group-hover:translate-x-0 group-hover:translate-y-0"></span>
-              <span class="absolute inset-0 w-full h-full border-4 border-black"></span>
-              <span class="relative">About Me</span>
+            <Link to="/aboutme" className="relative px-6 py-3 font-bold text-black group">
+              <span className="absolute inset-0 w-full h-full transition duration-300 ease-out transform -translate-x-2 -translate-y-2 bg-red-300 group-hover:translate-x-0 group-hover:translate-y-0"></span>
+              <span className="absolute inset-0 w-full h-full border-4 border-black"></span>
+              <span className="relative">About Me</span>
             </Link>
           </button>
         </div>
