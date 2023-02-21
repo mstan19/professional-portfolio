ec312a6540769b4774d97600a003de399b8ba065
melissa <melissastan91@gmail.com>
2023-02-20 22:24:23 -0500

    button display success messgae

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -5,16 +5,34 @@ import { ImPhone } from "react-icons/im";
 import { TfiEmail } from "react-icons/tfi";
 import { GrMapLocation } from "react-icons/gr";
 import emailjs from '@emailjs/browser';
+import Reaptcha from 'reaptcha';
+// require("dotenv").config();
+
 
 
 const Contact = () => {
 	const form = useRef();
 	const {register, formState: {errors}, handleSubmit} = useForm();
-	// const [userData, setUserData] = useState({
-	// 	name: "",
-	// 	email: "",
-	// 	message: ""
-	// });
+	const [buttonMessage, setButtonMessage]= useState(false);
+	const [captchaVerified, setCaptchaVerified] = useState(false);
+	const [emailSuccess, setEmailSuccess] = useState(false);
+
+	const onVerify = recaptchaResponse => {
+		setCaptchaVerified(true)
+	};
+
+	const textChange = () => {
+		setButtonMessage(!buttonMessage)
+	}
+	// const successMessage = async () => {
+	// 	return(
+	// 	<div class="bg-green-500 border-l-4 border-green-700 text-green-700 p-4" role="alert">
+	// 		<p class="font-bold">Success!</p>
+	// 		<p>Your message has been sent.</p>
+	// 	</div>
+	// 	)
+		
+	// };
 
 	const myContactInfomations = [
 		{
@@ -34,39 +52,32 @@ const Contact = () => {
 		},
 	]
 
-	const checkingAndSending = (event) => {
-		event.preventDefault();
-		// handleSubmit((data) => console.log(data));
-		if (!errors) {
-			sendEmail();
-		}
-	}
 
-	// update state based on form input changes
-	// const handleInputChange = (event) => {
-	// 	const { name, value } = event.target;
-	// 	setUserData({ ...userData, [name]: value });
-	// 	console.log(userData)
-	// };
-	console.log("errors", errors)
+console.log("errors", errors)
+	
 
-	const sendEmail = async (data) => {
-		 console.log(data)
-			emailjs.sendForm('service_9d0xx8q', 'template_1908rlg', form.current, '3j4PV5samtA2z-FeV')
-			.then((result) => {
-				console.log(result.text);
-				console.log("Message was sent!");
-			}, (error) => {
-				console.log(error.text);
-			});
-			console.log("at the end")
-	};
+const sendEmail = async (data) => {
+	console.log(data)
+	   emailjs.sendForm('service_9d0xx8q', 'template_1908rlg', form.current, '3j4PV5samtA2z-FeV')
+	   .then((result) => {
+		   console.log(result.text);
+		   console.log("Message was sent!");
+		   setEmailSuccess(true);
+	   }, (error) => {
+		   console.log(error.text);
+	   });
+	   console.log("at the end")
+};
 	return (
 		<div className="">
 			<h1 className="text-4xl font-normal text-center m-6">Let's Work Together!</h1>
 			<p className="text-lg m-6">Interested in working together? Message me using this form below. Speak to you soon!</p>
 			<div className="container mx-auto">
-				<form ref={form} onSubmit={handleSubmit(sendEmail)} className="flex flex-col items-center bg-white p-0 m-2 md:m-0">
+				<form 
+				ref={form} 
+				onSubmit={handleSubmit(sendEmail)} 
+
+				className="flex flex-col items-center bg-white p-0 m-2 md:m-0">
 					<div className="mb-4 md:w-1/2 w-full">
 						<label
 							className="block text-black-700 text-lg mb-2"
@@ -85,11 +96,10 @@ const Contact = () => {
 							className="appearance-none border border-black w-full py-2 px-3 text-black-700 leading-tight rounded-lg"
 							name="user_name"
 							type="text"
-							// onChange={handleInputChange}
 						></input>
 						{errors && errors.user_name?.message ? (
-                       <p className="text-red-700 bg-red-100 text-base border-solid border border-red-700 text-center p-7 m-2">{errors.user_name?.message}</p>
-                    ) : null}
+							<p className="text-red-700 bg-red-100 text-base border-solid border border-red-700 text-center p-7 m-2">{errors.user_name?.message}</p>
+						) : null}
 						
 					</div>
 					<div className="mb-4 md:w-1/2 w-full">
@@ -110,12 +120,11 @@ const Contact = () => {
 							className="appearance-none border border-black w-full py-2 px-3 text-black-700 leading-tight rounded-lg"
 							name="user_email"
 							type="text"
-							// onChange={handleInputChange}
 						></input>
 						
 						{errors && errors.user_email?.message ? (
-                       <p className="text-red-700 bg-red-100 text-base border-solid border border-red-700 text-center p-7 m-2">{errors.user_email?.message}</p>
-                    ) : null}
+							<p className="text-red-700 bg-red-100 text-base border-solid border border-red-700 text-center p-7 m-2">{errors.user_email?.message}</p>
+						) : null}
 					
 					</div>
 
@@ -140,23 +149,30 @@ const Contact = () => {
 							name="message" 
 							id="message" 
 							type="text" 
-							// onChange={handleInputChange}
 						>
 						</textarea>
 						
 						{errors && errors.message?.message ? (
-                        <p className="text-red-700 bg-red-100 text-base border-solid border border-red-700 text-center p-7 m-2">{errors.message?.message}</p>
-                    ) : null}
+							<p className="text-red-700 bg-red-100 text-base border-solid border border-red-700 text-center p-7 m-2">{errors.message?.message}</p>
+						) : null}
 					
 					</div>
+
+					<Reaptcha 
+					sitekey="6LcDxpwkAAAAAMYeHaSLMfrnTZbz1mZXAnka-o1B" 
+					onVerify={onVerify} 
+					/>
+
 					<button
-						className="bg-red-300 w-1/3 shadow-lg rounded-lg hover:bg-red-400 text-white py-2 px-4 focus:outline-none focus:shadow-outline"
+						className={`w-1/3 shadow-lg rounded-lg hover:bg-red-400 text-white mt-4 py-2 px-4 focus:outline-none focus:shadow-outline ${emailSuccess ? "bg-emerald-500 hover:bg-green-400" : "bg-red-300 hover:bg-red-400"} `}
 						type="submit"
-						value="Send" 
+						value="Send"
+						disabled={!captchaVerified}
 					>
-						SUBMIT
+						{buttonMessage ? "SUBMIT" : "SUCCESS"}
 					</button>
 				</form>
+				
 				<div className="grid grid-cols-1 md:grid-cols-3 mt-16 mb-28 md:mb-0">
 					{myContactInfomations.map((myContact) => (
 						<div className="text-2xl" key={myContact.info}>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import pic from "./../assets/flower.jpg";
+import pic from "./../assets/cherrybloosom.jpeg";
 
 
 const Home = () => {
@@ -15,15 +15,18 @@ const Home = () => {
 
     return (
         <div className="flex flex-col justify-center text-center">
-          <h1 className="text-5xl font-semibold mb-2">Melissa Stan</h1>
-          <p className="text-2xl text-gray-500 font-medium">Full Stack Web Developer</p>
-          <button className="mt-10">
-            <Link to="/aboutme" className="relative px-6 py-3 font-bold text-black group">
-              <span className="absolute inset-0 w-full h-full transition duration-300 ease-out transform -translate-x-2 -translate-y-2 bg-red-300 group-hover:translate-x-0 group-hover:translate-y-0"></span>
-              <span className="absolute inset-0 w-full h-full border-4 border-black"></span>
-              <span className="relative">About Me</span>
-            </Link>
-          </button>
+          <img className="relative object-cover h-full" src={pic} alt="backgroundPic"></img>
+          <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white p-5 opacity-60">
+            <h1 className="text-5xl font-semibold mb-2">Melissa Stan</h1>
+            <p className="text-2xl text-gray-500 font-medium">Full Stack Web Developer</p>
+            <button className="mt-10">
+              <Link to="/aboutme" className="relative px-6 py-3 font-bold text-black group">
+                <span className="absolute inset-0 w-full h-full transition duration-300 ease-out transform -translate-x-2 -translate-y-2 bg-red-300 group-hover:translate-x-0 group-hover:translate-y-0"></span>
+                <span className="absolute inset-0 w-full h-full border-4 border-black"></span>
+                <span className="relative">About Me</span>
+              </Link>
+            </button>
+          </div>
         </div>
     );
 };
