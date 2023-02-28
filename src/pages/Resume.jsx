8decb9b74ccfe6f34fadfe3c3d8e7e6760aef49a
melissa <melissastan91@gmail.com>
2023-02-27 21:12:30 -0500

    resume is mobile friendly

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -36,7 +36,7 @@ const Resume = () => {
 				</section>
 
 				{/* Projects */}
-				<section className="h-52 md:col-span-2 row-span-2 md:h-[34rem] group relative cursor-pointer items-center justify-center overflow-hidden transition-shadow hover:shadow-xl hover:shadow-black/30 group [prespective:1000px]">
+				<section className="h-[34rem] md:col-span-2 row-span-2 md:h-[34rem] group relative cursor-pointer items-center justify-center overflow-hidden transition-shadow hover:shadow-xl hover:shadow-black/30 group [prespective:1000px]">
 					<img className="h-full w-full object-cover transition-transform duration-500 group-hover:scale-125" src={projectPic} alt="projects" />
 					<div className="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black group-hover:from-black/70 group-hover:via-black/60 group-hover:to-black/70"></div>
 					<div className="absolute inset-0 flex translate-y-[60%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-0">
@@ -46,12 +46,16 @@ const Resume = () => {
 						<p className="mb-3 text-lg italic text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100 pb-5">MeowWoof: Created a RESTful API that followed a MVC paradigm, which is designed to help keeping track of animals in a pet shelter.</p>
 						<p className="mb-3 text-lg italic text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100 pb-">TurnipFest: Built an interactive website that generates a recipe based on the user's special diet preferences and choosing music genres.</p>
 
-						<button className="border-2 border-slate-100 rounded-full bg-neutral-900 py-2 px-3.5 text-sm capitalize text-white shadow shadow-black/60"><Link to="/projects"></Link>See More</button>
+						<button className="border-2 border-slate-100 rounded-full bg-neutral-900 py-2 px-3.5 text-sm capitalize text-white shadow shadow-black/60">
+							<Link to="/projects" className="">
+								<div>See More</div>
+							</Link>
+						</button>
 					</div>
 				</section>
 
 				{/* Skills */}
-				<section className="h-52 md:row-span-2 md:h-[40rem]
+				<section className="h-[20rem] md:row-span-2 md:h-[40rem]
 				group relative cursor-pointer items-center justify-center overflow-hidden transition-shadow hover:shadow-xl hover:shadow-black/30 group [prespective:1000px]">
 					<img className="h-full w-full object-cover transition-transform duration-500 group-hover:scale-125" src={skillsPic} alt="skills" />
 					<div className="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black group-hover:from-black/70 group-hover:via-black/60 group-hover:to-black/70"></div>
@@ -62,7 +66,7 @@ const Resume = () => {
 					</div>
 				</section>
 				{/* Education */}
-				<section className="h-52 md:h-80 group relative cursor-pointer items-center justify-center overflow-hidden transition-shadow hover:shadow-xl hover:shadow-black/30 group [prespective:1000px]">
+				<section className="h-[20rem] md:h-80 group relative cursor-pointer items-center justify-center overflow-hidden transition-shadow hover:shadow-xl hover:shadow-black/30 group [prespective:1000px]">
 					<img className="h-full w-full object-cover transition-transform duration-500 group-hover:scale-125" src={gradCap} alt="education" />
 					<div className="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black group-hover:from-black/70 group-hover:via-black/60 group-hover:to-black/70"></div>
 					<div className="absolute inset-0 flex translate-y-[60%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-0">
@@ -79,7 +83,7 @@ const Resume = () => {
 				</section>
 
 				{/* Experience */}
-				<section className="h-52 md:h-80 group relative cursor-pointer items-center justify-center overflow-hidden transition-shadow hover:shadow-xl hover:shadow-black/30 group [prespective:1000px]">
+				<section className="h-[20rem] md:h-80 group relative cursor-pointer items-center justify-center overflow-hidden transition-shadow hover:shadow-xl hover:shadow-black/30 group [prespective:1000px]">
 					<img className="h-full w-full object-cover transition-transform duration-500 group-hover:scale-125" src={experiencePic} alt="experience" />
 					<div className="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black group-hover:from-black/70 group-hover:via-black/60 group-hover:to-black/70"></div>
 					<div className="absolute inset-0 flex translate-y-[60%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-0">
