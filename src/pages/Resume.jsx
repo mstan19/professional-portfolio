c1a4c4d393a862b273688979e185ed938409fe44
melissa <melissastan91@gmail.com>
2023-09-10 07:57:04 -0400

    added the java cert

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -8,6 +8,7 @@ import experiencePic from "./../assets/experiencePic.jpg";
 import { TfiDownload } from "react-icons/tfi";
 import pdf from "./../assets/resume.pdf";
 import bootcampCertificate from "./../assets/bootcampCertificate.pdf";
+import javaCert from "./../assets/javaCert.pdf"
 import skillsPic from "./../assets/typewriterPic.jpg";
 
 
@@ -39,7 +40,7 @@ const Resume = () => {
 				<section className="h-[34rem] md:col-span-2 row-span-2 md:h-[34rem] group relative cursor-pointer items-center justify-center overflow-hidden transition-shadow hover:shadow-xl hover:shadow-black/30 group [prespective:1000px]">
 					<img className="h-full w-full object-cover transition-transform duration-500 group-hover:scale-125" src={projectPic} alt="projects" />
 					<div className="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black group-hover:from-black/70 group-hover:via-black/60 group-hover:to-black/70"></div>
-					<div className="absolute inset-0 flex translate-y-[60%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-0">
+					<div className="absolute inset-0 flex translate-y-[70%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-0">
 						<h1 className="text-3xl font-bold text-white mb-5">Projects</h1>
 						<p className="mb-3 text-lg italic text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100 pb-5">Cozia: Developed a MERN stack apparel e-commerce application.</p>
 						<p className="mb-3 text-lg italic text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100 pb-5">ResuMade: Directed an e-commerce MERN stack app that implemented the MVC paradigm to produce a formatted resume based on users information. </p>
@@ -59,24 +60,31 @@ const Resume = () => {
 				group relative cursor-pointer items-center justify-center overflow-hidden transition-shadow hover:shadow-xl hover:shadow-black/30 group [prespective:1000px]">
 					<img className="h-full w-full object-cover transition-transform duration-500 group-hover:scale-125" src={skillsPic} alt="skills" />
 					<div className="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black group-hover:from-black/70 group-hover:via-black/60 group-hover:to-black/70"></div>
-					<div className="absolute inset-0 flex translate-y-[60%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-0">
+					<div className="absolute inset-0 flex translate-y-[63%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-0">
 						<h1 className="text-3xl font-bold text-white mb-10">Skills</h1>
-						<div className="mb-3 text-lg italic text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100"><div className="underline underline-offset-1">LANGUAGES:</div> HTML5, CSS3, JavaScript (ES6), MySQL, NoSQL, GraphQL, jQuery</div>
-						<div className="mb-3 text-lg italic text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100"><div className="underline underline-offset-1">FRAMEWORKS:</div>CSS Frameworks, Express, Sequelize, MongoDB and Mongoose, Apollo, React, APIs, Agile Methodology, Github, and Deployment (AWS, Heroku)</div>
+						<div className="mb-3 text-lg italic text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100"><div className="underline underline-offset-1">LANGUAGES:</div> CSS3, Git, GraphQL, HTML5, Java, JavaScript (ES6), jQuery, MySQL, NoSQL</div>
+						<div className="mb-3 text-lg italic text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100"><div className="underline underline-offset-1">FRAMEWORKS:</div>Agile Methodology, APIs, Apollo, CSS Frameworks, Deployment (AWS, Heroku), Express, Github, MongoDB, Mongoose, React, and Sequelize </div>
 					</div>
 				</section>
 				{/* Education */}
 				<section className="h-[20rem] md:h-80 group relative cursor-pointer items-center justify-center overflow-hidden transition-shadow hover:shadow-xl hover:shadow-black/30 group [prespective:1000px]">
 					<img className="h-full w-full object-cover transition-transform duration-500 group-hover:scale-125" src={gradCap} alt="education" />
 					<div className="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black group-hover:from-black/70 group-hover:via-black/60 group-hover:to-black/70"></div>
-					<div className="absolute inset-0 flex translate-y-[60%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-0">
-						<h1 className="text-3xl font-bold text-white mb-8">Education</h1>
-						<p className="mb-2 text-lg italic text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100 pb-2">Certificate in Full Stack Web Development: Georgia Institute of Technology</p>
-						<p className="mb-2 text-lg italic text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100">BS in Psychology and Minor in Biological Science degree with research distinction: Georgia State University</p>
+					<div className="absolute inset-0 flex translate-y-[85%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:-translate-y-4">
+						<h1 className="text-3xl font-bold text-white">Education</h1>
+						{/* <div className=""> */}
+							<p className="mb-2 text-lg italic text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100">Java SE 8 Oracle Certified Associate (OCA) Certification</p>
+							<div className="flex inline mb-4">
+								<button className="flex inline border-2 border-slate-100 rounded-full bg-neutral-900 py-2 px-3.5 text-sm capitalize text-white shadow shadow-black/60"><Link to={javaCert} download="Melissa Stan Java Certificate" target="_blank" rel="noreferrer">Java Certificate</Link></button>
+							</div>
+							<p className="mb-2 text-lg italic text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100 pb-2">Full Stack Web Development Certificate: Georgia Institute of Technology</p>
+							<div className="flex inline">
+								<button className="flex inline border-2 border-slate-100 rounded-full bg-neutral-900 py-2 px-3.5 text-sm capitalize text-white shadow shadow-black/60"><Link to={bootcampCertificate} download="Melissa Stan Certificate" target="_blank" rel="noreferrer">Certificate</Link></button>
+							</div>
+							<p className="mb-2 text-lg italic text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100">BS in Psychology and Minor in Biological Science Degree with Research Distinction: Georgia State University</p>
+						{/* </div> */}
 
-						<div className="flex inline">
-							<button className="flex inline border-2 border-slate-100 rounded-full bg-neutral-900 py-2 px-3.5 text-sm capitalize text-white shadow shadow-black/60"><Link to={bootcampCertificate} download="Melissa Stan Certificate" target="_blank" rel="noreferrer">Certificate</Link></button>
-						</div>
+						
 
 					</div>
 					Education
@@ -86,11 +94,11 @@ const Resume = () => {
 				<section className="h-[20rem] md:h-80 group relative cursor-pointer items-center justify-center overflow-hidden transition-shadow hover:shadow-xl hover:shadow-black/30 group [prespective:1000px]">
 					<img className="h-full w-full object-cover transition-transform duration-500 group-hover:scale-125" src={experiencePic} alt="experience" />
 					<div className="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black group-hover:from-black/70 group-hover:via-black/60 group-hover:to-black/70"></div>
-					<div className="absolute inset-0 flex translate-y-[60%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-0">
+					<div className="absolute inset-0 flex translate-y-[65%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-0">
 						<h1 className="text-3xl font-bold text-white mb-10">Experience</h1>
-						<p className="mb-2 text-lg italic text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100 pb-2">Medical Assistant at Gadlage and Associates Clinic, Atlanta, GA</p>
-						<p className="mb-2 text-lg italic text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100">Medical Assistant at Test4Free Clinic, Atlanta, GA</p>
-						<p className="mb-2 text-lg italic text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100 pt-2">Certified Nursing Assistant at Christian Home, Loganville, GA</p>
+						<p className="mb-2 text-lg italic text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100 pb-2">Lead Code Coach at the Coder School, Suwannee, GA</p>
+						<p className="mb-2 text-lg italic text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100">Code Coach at the Coder School, Suwannee, GA</p>
+						<p className="mb-2 text-lg italic text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100 pt-2">Full Stack Web Developer Freelance, Atlanta, GA</p>
 					</div>
 					Experience
 				</section>
