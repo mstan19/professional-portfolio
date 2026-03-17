5fb81ca8d6f969c3689cae09565c4ccc0dec0b94
jli439 <jack.li@gtri.gatech.edu>
2026-03-16 21:33:21 -0400

    adding docker compose for app deploy

--- aws/deploy/README.md
+++ aws/deploy/README.md
@@ -2,4 +2,3 @@ prerequisites:
 
 instructions:
 once the ec2 instance is deployed, login to it via ssm and run:
-
--- aws/modules/EC2/scripts/user_data.sh
+++ aws/modules/EC2/scripts/user_data.sh
@@ -12,3 +12,9 @@ mkdir -p ~/.docker/cli-plugins
 curl -SL "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o ~/.docker/cli-plugins/docker-compose
 chmod +x ~/.docker/cli-plugins/docker-compose
 docker compose version
+
+# install git
+dnf install git -y
+
+# install node
+dnf install nodejs20 -y
