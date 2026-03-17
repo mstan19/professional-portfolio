86d423503341263fa7a946a71e96e2ef26519dbd
jli439 <jack.li@gtri.gatech.edu>
2026-03-16 23:03:51 -0400

    typo

--- aws/modules/EC2/scripts/user_data.sh
+++ aws/modules/EC2/scripts/user_data.sh
@@ -16,7 +16,7 @@ docker compose version
 
 # update docker buildx
 VERSION=$(curl -s https://api.github.com/repos/docker/buildx/releases/latest | jq -r .tag_name)
-wget https://github.com/docker/buildx/releases/download/${VERSION}/buildx-${VERSION}.linux-amd64 -O buildx
+wget https://github.com/docker/buildx/releases/download/${VERSION}/buildx-${VERSION}.linux-arm64 -O buildx
 mv buildx ~/.docker/cli-plugins/docker-buildx
 chmod +x ~/.docker/cli-plugins/docker-buildx
 # install git
