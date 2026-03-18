e7d645e26a4816ad1d3468d4021a76f862bea6cf
jli439 <jack.li@gtri.gatech.edu>
2026-03-18 18:23:35 -0400

    adding fixing ssm-user cmds

--- aws/modules/EC2/scripts/user_data.sh
+++ aws/modules/EC2/scripts/user_data.sh
@@ -22,23 +22,37 @@ curl -SL "https://github.com/docker/compose/releases/latest/download/docker-comp
 chmod +x ~/.docker/cli-plugins/docker-compose
 docker compose version
 # for ssm-user
-mkdir -p /home/ssm-user/.docker/cli-plugins
+sudo -u ssm-user bash <<EOF
+mkdir -p ~/.docker/cli-plugins
 curl -SL "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o ~/.docker/cli-plugins/docker-compose
-chmod +x /home/ssm-user/.docker/cli-plugins/docker-compose
-
+chmod +x ~/.docker/cli-plugins/docker-compose
+docker compose version
+EOF
 
 # update docker buildx
 VERSION=$(curl -s https://api.github.com/repos/docker/buildx/releases/latest | jq -r .tag_name)
 wget https://github.com/docker/buildx/releases/download/${VERSION}/buildx-${VERSION}.linux-arm64 -O buildx
-mv buildx /home/ssm-user//.docker/cli-plugins/docker-buildx
-chmod +x /home/ssm-user//.docker/cli-plugins/docker-buildx
+mv buildx /home/ssm-user/.docker/cli-plugins/docker-buildx
+chmod +x /home/ssm-user/.docker/cli-plugins/docker-buildx
 # for ssm-user
+sudo -u ssm-user bash <<EOF
+VERSION=$(curl -s https://api.github.com/repos/docker/buildx/releases/latest | jq -r .tag_name)
 wget https://github.com/docker/buildx/releases/download/${VERSION}/buildx-${VERSION}.linux-arm64 -O buildx
-mv buildx /home/ssm-user//.docker/cli-plugins/docker-buildx
-chmod +x /home/ssm-user//.docker/cli-plugins/docker-buildx
+mv buildx /home/ssm-user/.docker/cli-plugins/docker-buildx
+chmod +x /home/ssm-user/.docker/cli-plugins/docker-buildx
+EOF
 
 # install git
 dnf install git -y
 
 # install node
 dnf install nodejs20 -y
+
+sudo -u ssm-user bash <<EOF
+git clone https://github.com/mstan19/professional-portfolio.git /home/ssm-user/professional-portfolio
+cd /home/ssm-user/professional-portfolio
+git checkout aws-deploy
+npm install
+docker compose up -d
+EOF
+
--- aws/modules/EC2/security_groups.tf
+++ aws/modules/EC2/security_groups.tf
@@ -9,12 +9,18 @@ resource "aws_security_group" "this" {
 
 resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
   security_group_id = aws_security_group.this.id
-  cidr_ipv4         = data.aws_vpc.default.cidr_block
+  cidr_ipv4         = "0.0.0.0/0"
   from_port         = 443
   ip_protocol       = "tcp"
   to_port           = 443
 }
-
+resource "aws_vpc_security_group_ingress_rule" "allow_tcp_ipv4" {
+  security_group_id = aws_security_group.this.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 80
+  ip_protocol       = "tcp"
+  to_port           = 80
+}
 # resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv6" {
 #   security_group_id = aws_security_group.this.id
 #   cidr_ipv6         = data.aws_vpc.default.ipv6_cidr_block
