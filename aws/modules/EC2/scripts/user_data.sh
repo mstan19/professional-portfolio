bff580e8770794b75b1ab92183a71b1ea65f76e7
jli439 <jack.li@gtri.gatech.edu>
2026-03-18 17:26:04 -0400

    adding ssm-user to user data

--- aws/modules/EC2/scripts/user_data.sh
+++ aws/modules/EC2/scripts/user_data.sh
@@ -6,6 +6,14 @@ dnf update -y
 dnf install -y docker
 systemctl start docker
 systemctl enable docker
+
+if ! id -u ssm-user &>/dev/null; then
+    useradd -m ssm-user
+    # CRITICAL: Grant passwordless sudo access (mimic default SSM behavior)
+    echo "ssm-user ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/ssm-agent-users
+    chmod 440 /etc/sudoers.d/ssm-agent-users
+fi
+
 usermod -aG docker ssm-user
 
 # install docker compose
@@ -13,15 +21,24 @@ mkdir -p ~/.docker/cli-plugins
 curl -SL "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o ~/.docker/cli-plugins/docker-compose
 chmod +x ~/.docker/cli-plugins/docker-compose
 docker compose version
+# for ssm-user
+mkdir -p /home/ssm-user/.docker/cli-plugins
+curl -SL "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o ~/.docker/cli-plugins/docker-compose
+chmod +x /home/ssm-user/.docker/cli-plugins/docker-compose
+
 
 # update docker buildx
 VERSION=$(curl -s https://api.github.com/repos/docker/buildx/releases/latest | jq -r .tag_name)
 wget https://github.com/docker/buildx/releases/download/${VERSION}/buildx-${VERSION}.linux-arm64 -O buildx
-mv buildx ~/.docker/cli-plugins/docker-buildx
-chmod +x ~/.docker/cli-plugins/docker-buildx
+mv buildx /home/ssm-user//.docker/cli-plugins/docker-buildx
+chmod +x /home/ssm-user//.docker/cli-plugins/docker-buildx
+# for ssm-user
+wget https://github.com/docker/buildx/releases/download/${VERSION}/buildx-${VERSION}.linux-arm64 -O buildx
+mv buildx /home/ssm-user//.docker/cli-plugins/docker-buildx
+chmod +x /home/ssm-user//.docker/cli-plugins/docker-buildx
+
 # install git
 dnf install git -y
 
 # install node
 dnf install nodejs20 -y
-
